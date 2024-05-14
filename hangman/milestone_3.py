af366b1719ccb8fab90645ffc5128c79d007ce11
LyatifAhmed <latifmutlu@gmail.com>
2024-05-14 20:21:18 +0100

    Adds a file milestone_3.py

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,11 +1,24 @@
-while True:
-    guess = input("Guess a letter:")
-    if guess.isalpha():
-        break
-    print("Invalid letter. Please, enter a single alphabethical character.")
-
-    if guess in word:
+def check_guess(guess):
+    
+    if guess.lower() in word:
         print(f"Good guess!{guess} is in the word.")
     else:
         print(f"Sorry,{guess} is not in the word. Try again.")
+
+def ask_for_input():
+    
+    while True:
+
+       guess = input("Guess a letter:")
+       if guess.isalpha():
+           
+           break
+    print("Invalid letter. Please, enter a single alphabethical character.") 
+
+    check_guess(guess)
+    
+ask_for_input()
+
+
+
             
