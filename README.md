e9bc2949b8ceedeb3c1a7ada18f05c0f38a29b6e
LyatifAhmed <latifmutlu@gmail.com>
2024-05-21 09:43:25 +0100

    adds file milestone_3.py and updates readme

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Hangman is a classic game in which a player thinks of a word and the other playe
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
 
-- File milestone_2 asks an input from user and checks that input is make sense.
+- File milestone_2 asks an input from user and checks that input makes sense.
+- file milestone_3 that involves 2 function check_guess() and ask_for_input().
 
 
 
--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,10 +1,15 @@
-import milestone_2
+
 
 def check_guess(guess):
+
     guess = guess.lower()
+
     if guess in word:
+
         print(f"Good guess!{guess} is in the word.")
+
     else:
+        
         print(f"Sorry,{guess} is not in the word. Try again.")
 
 def ask_for_input():
@@ -12,10 +17,14 @@ def ask_for_input():
     while True:
 
        guess = input("Guess a letter:")
-       if guess.isalpha():
+
+       if len(guess) == 1 and guess.isalpha():
            
            break
-    print("Invalid letter. Please, enter a single alphabethical character.") 
+       
+       else:
+          
+          print("Invalid letter. Please, enter a single alphabethical character.") 
 
     check_guess(guess)
 
